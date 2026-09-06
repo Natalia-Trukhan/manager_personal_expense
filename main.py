9ffd0aae1054bd38623b8f39a248d5d4dd337023
Natalia-Trukhan <truhannatali1987@gmail.com>
2026-09-06 22:09:23 +0200

    chenge a case 1 and the beginning of the file main

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import logging
 
+from expense import Expense, OperationsUnderExpense
+
 logging.basicConfig(
     filename="app.log",
     level=logging.INFO,
@@ -7,28 +9,46 @@ logging.basicConfig(
     encoding="utf-8",
 )
 
+operation_under_expense = OperationsUnderExpense()
+
 def main():
     logging.info("Starting the program.")
     initial_info()
     while True:
-        try:
-            menu()
-            selected_option: int = int(input("Your choice: "))
-            if not selected_option:
-                raise ValueError("Choice must be digit only between 1 to 5!") 
-            elif selected_option>5 or selected_option<1:
-                raise ValueError("Choice must be only between 1 to 5!")
-            else:
+        while True:
+            try:
+                menu()
+                selected_option: int = int(input("Your choice: "))
+                if selected_option>5 or selected_option<1:
+                    raise ValueError("Choice must be only between 1 to 5!")
                 break
 
-        except Exception as error:
-            print(error)
-            logging.error(error)
-            continue
+            except Exception as error:
+                print(f"Error: {error}")
+                logging.error(f"Input error in menu: {error}")
+                continue
 
-    match selected_option:
-        case 1:
-            pass
+        match selected_option:
+            case 1:
+                logging.info("User is adding a new expense.")
+                try:
+                    print("You must enter the title, the amount, the category of expense.")
+                    title: str = input("Title: ")
+                    amount: float = float(input("Amount: "))
+                    category: str = input("Category(food, transport): ")
+                    category_split = category.split(",")
+                    id_new_expense = operation_under_expense.id_number()
+
+                    new_expense = Expense(title, amount, category_split, id_new_expense )
+
+                    operation_under_expense.add_expense(new_expense)
+                    print("The new expense was added successfully!")
+                    logging.info(
+                        f"Expense '{title}' for amount {amount} successfully added."
+                    )
+                except Exception as error:
+                    print(f"Error: {error}")
+
 
 def initial_info():
     print("""
